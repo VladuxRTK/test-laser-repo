691b1e63b5fe3403744d77a8667290a2abcb6d95
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-04-02 20:20:19 +0300

    yeah

---
+++ LineRendererTest/Assets/ControlLasers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLasers : MonoBehaviour
+{
+    public Laser laser1;
+    public Laser laser2;
+    public Laser laser3;
+    private bool isRunning;
+    int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(LaserShooting());
+        
+      
+    }
+
+    private IEnumerator LaserShooting()
+    {
+        //int count = 0;
+        if(Input.GetKey(KeyCode.Space))
+        {
+
+               
+                StartCoroutine(laser1.LaserShot());
+                yield return StartCoroutine(laser2.LaserShot());
+            StartCoroutine(laser3.LaserShot());
+            
+
+
+        }
+    }
+    private IEnumerator Shoot()
+    {
+        int i = 0;
+        while(i<2)
+        {
+            StartCoroutine(laser1.LaserShot());
+            yield return StartCoroutine(laser2.LaserShot());
+            i++;
+        }
+
+    }
+}
---
+++ LineRendererTest/Assets/Laser.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform laserHit;
+    // public Transform laserHit2;
+    //   public Transform laser1;
+    // public Transform laser2;
+    private int count;
+
+    Vector3 d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
+        d = transform.up;
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        //Destroy(hit.transform.gameObject);
+
+        //RaycastHit2D hit1 = Physics2D.Raycast(transform.position, -transform.up);
+        // laserHit.position = hit.point;
+        // laserHit.position = hit.point;
+        // laserHit2.position = hit1.point;
+        lineRenderer.SetPosition(0, this.transform.position);
+        lineRenderer.SetPosition(1, laserHit.position);
+      
+       /* lineRenderer.SetPosition(2, laser2.position);
+        lineRenderer.SetPosition(3, laserHit2.position);*/
+        
+        
+        //Debug.DrawLine
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+            Destroy(collision.gameObject);
+    }*/
+
+    public IEnumerator LaserShot()
+    {
+
+
+    
+            
+            lineRenderer.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+            lineRenderer.enabled = false;
+            yield return new WaitForSeconds(0.5f);
+            lineRenderer.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+            lineRenderer.enabled = false;   
+           
+        
+
+
+
+
+    }
+
+}
